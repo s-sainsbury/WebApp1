d7930d993f521f482228efcf7d939f39dcd34454
s-sainsbury <s_sainsbury@hotmail.com>
2023-04-03 13:39:55 +0000

    unit test

---
+++ WebTest/TestProject1/WebTest.cs
@@ -0,0 +1,14 @@
+namespace TestProject1
+{
+    public class WebTest
+    {
+        [Fact]
+        public void DemoTest()
+        {
+            int i = 1;
+            bool result = false;
+            if (i == 1) result = true;
+            Assert.True(result, "value should be equal to 1");
+        }
+    }
+}
